465b6ed23760d0128119aac8bd1c8c9a5b37547a
Abhishek <abhishek@fisdom.com>
2021-04-01 12:46:47 +0530

    add border inline style

--- src/Components/Helloworld/index.jsx
+++ src/Components/Helloworld/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function HelloWorld() {
-  return <div>Hello World</div>
+  return <div style={{padding:12,border :'1px solid black'}}>Hello World</div>
 }
 
 export default HelloWorld;
